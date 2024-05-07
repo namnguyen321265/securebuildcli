750a568613b742c79cd88f7041fc9e830c555dc4
namnguyen321265 <bernakayisnam1311@outlook.com>
2024-05-07 15:35:03 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const difference = (a, b) => {
-  const s = new Set(b);
-  return a.filter((x) => !s.has(x));
-};
+function moveZeroes(nums) {
+  let index = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[index++] = nums[i];
+    }
+  }
+  for (let i = index; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+  return nums;
+}
